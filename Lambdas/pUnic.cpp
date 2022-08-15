84b7190c492f9f3c944d42ec9a266beca292570b
Andrei2458 <andrei.vacariu@student.upt.ro>
2022-08-15 11:54:45 +0000

    files for explaining lambdas

---
+++ Lambdas/pUnic.cpp
@@ -0,0 +1,6 @@
+#include "Resource.hpp"
+#include <memory>
+
+int main() {
+    auto pResource = std::make_unique<Resource>(", ");
+}
--- Lambdas/tiny.cpp
+++ Lambdas/tiny.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include "Resource.hpp"
+#include <memory>
 
 int main() {
     auto isOdd = [](int candidate) {return candidate %2 != 0; };
@@ -17,5 +19,51 @@ int main() {
         [](int elem) { return elem %2 == 0; });
     std::cout << "# of odd elems: " << odds << std::endl;
     std::cout << "# of even elems: " << evens << std::endl;
+
+    ///////////////////
+
+	//capturing by value and reference explicitly  [x,y,&message]
+
+    int x = 3;
+    int y = 7;
+
+    std::string message = "\nelements between ";
+    message += std::to_string(x) + " and " + std::to_string(y) + " inclusive: ";
+    for_each(begin(nums), end(nums),
+        [x, y, &message](int n) {
+            if (n >= x && n <= y) {
+                message += " " + std::to_string(n);
+            }
+        });
+    message += "\n";
+    std::cout << message;
+
+    //////////////////////
+
+    x = y = 0;
+    // x by value, rest is by refference
+    for_each(begin(nums), end(nums),
+        [&, x](int elem) mutable {
+            x += elem;
+            y += elem;
+        });
+    
+    std::cout << "x = " << x << "\n" << "y = " << y << "\n";
+
+    //////////////////////
+    // { // braces for scope
+    //     auto pResource = std::make_unique<Resource>(", "); // a smart pointer that automatically clean itself up
+
+    //     for_each(begin(nums), end(nums), 
+    //         [=, &message, p = std::move(pResource)](int n) {
+    //             if (n >= x && n <=y ) {
+    //                 message += p->getName() + std::to_string(n);
+    //             }
+    //         });
+        
+    //     // pResource is not out of scope yet -> but it was moved to the lambda so it is empty now
+    //     std::cout << "\nOut of for_each\n";
+    // }
+    // std::cout << "\nOut of scope from braces\n";
 
 }
